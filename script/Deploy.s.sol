247bc73774f58e0790827dfc77689d34d1679886
Abdulmalik Muhammad <amalikmuhdd@gmail.com>
2026-06-11 23:55:32 +0100

    feat: deploy and wire RewardNFT in deploy script
    After deploying StakingRewards, deploy RewardNFT with the staking
    contract as minter and call setRewardNFT to link them. Log both
    deployed addresses.

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,26 +3,31 @@ pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 import {StakingRewards} from "../src/StakingRewards.sol";
+import {RewardNFT} from "../src/RewardNFT.sol";
 
-/// @notice Deploys StakingRewards using addresses from the environment.
+/// @notice Deploys StakingRewards and RewardNFT, then wires them together.
 /// @dev Required env vars:
 ///        OWNER          - address that can notify rewards / set duration
 ///        STAKING_TOKEN  - ERC20 users stake
 ///        REWARDS_TOKEN  - ERC20 paid out as rewards
+///      The broadcasting account must be OWNER, since setRewardNFT is onlyOwner.
 ///      Example:
 ///        forge script script/Deploy.s.sol:DeployStakingRewards \
 ///          --rpc-url $RPC_URL --broadcast --verify
 contract DeployStakingRewards is Script {
-    function run() external returns (StakingRewards staking) {
+    function run() external returns (StakingRewards staking, RewardNFT rewardNFT) {
         address owner = vm.envAddress("OWNER");
         address stakingToken = vm.envAddress("STAKING_TOKEN");
         address rewardsToken = vm.envAddress("REWARDS_TOKEN");
 
         vm.startBroadcast();
         staking = new StakingRewards(owner, stakingToken, rewardsToken);
+        rewardNFT = new RewardNFT(address(staking));
+        staking.setRewardNFT(address(rewardNFT));
         vm.stopBroadcast();
 
         console.log("StakingRewards deployed at:", address(staking));
+        console.log("RewardNFT deployed at:     ", address(rewardNFT));
         console.log("  owner:        ", owner);
         console.log("  stakingToken: ", stakingToken);
         console.log("  rewardsToken: ", rewardsToken);
